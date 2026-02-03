164bd4f04472ec9c8b7c62922aa88927d4f9f9c2
bohan0 <bh.inbox@gmail.com>
2026-02-03 15:12:12 +1100

    Update test_endeavour.py - add test for N71 solar soak
    add test for beginning of period at 10:06am and end of period at 1:54pm

--- test/test_endeavour.py
+++ test/test_endeavour.py
@@ -72,6 +72,22 @@ class TestEndeavour(unittest.TestCase):
         price = convert(interval_time, tariff_code, rrp)
         self.assertAlmostEqual(price, expected_price, places=2)
 
+    def test_convert_N71_solar_soak_10am(self):
+        interval_time = datetime(2026, 2, 3, 10, 6, tzinfo=ZoneInfo(time_zone()))
+        tariff_code = 'N71'
+        rrp = -3.425
+        expected_price = 0.0
+        price = convert(interval_time, tariff_code, rrp)
+        self.assertAlmostEqual(price, expected_price, places=4)
+
+    def test_convert_N71_solar_soak_1pm(self):
+        interval_time = datetime(2026, 2, 3, 13, 54, tzinfo=ZoneInfo(time_zone()))
+        tariff_code = 'N71'
+        rrp = -3.425
+        expected_price = 0.0
+        price = convert(interval_time, tariff_code, rrp)
+        self.assertAlmostEqual(price, expected_price, places=4)
+
     def test_convert_unknown_tariff(self):
         interval_time = datetime(2023, 7, 15, 10, 0, tzinfo=ZoneInfo(time_zone()))
         tariff_code = 'N999'
